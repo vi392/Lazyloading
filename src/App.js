6056167d9303e05fa2fed2d746c64a3810b0587f
vi392 <vishalkrdwiwedi@gmail.com>
2025-01-30 12:15:53 +0530

    lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Suspense } from 'react'
+// import Lazyloading from './Lazyloading/Lazyloading'
+
+const Lazy= React.lazy(()=>import('./lazylod/Lazyloading')) 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h2>Hello ji lazy loading </h2> 
+    <Suspense fallback= {<div><h2>loading..</h2></div>}>
+    <Lazy/>
+    </Suspense>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/lazylod/Lazyloading.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+function Lazyloading() {
+  return (
+   <>
+   
+   <button>Add</button> <br></br>
+   <button>Subtract</button> 
+
+   <p>
+   React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js and Remix[a]. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[11][12] A key advantage of React is that it only re-renders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements.
+   </p> 
+   <br>
+   </br>
+
+   <p>
+   React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js and Remix[a]. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[11][12] A key advantage of React is that it only re-renders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements.
+   </p>
+
+   </>
+  )
+}
+
+export default Lazyloading
